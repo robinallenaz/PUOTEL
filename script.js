c556aeb76de52aeb5bc7dab1bc9d400ea2dbea38
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-05 22:25:31 -0700

    Created function to increment variable and call itself recursively with a try-catch block and made flatten function

---
+++ script.js
@@ -0,0 +1,33 @@
+//Declare a global counter variable
+
+let i = 0;
+
+// Create a simple function that increments the variable, and then calls itself recursively.
+function simpleFunction() {
+  i++;
+  simpleFunction();
+}
+// Surround the initial function call in a try/catch block.
+try {
+  simpleFunction();
+} catch (err) {
+  console.log(err);
+  console.log(i);
+}
+
+// Within the catch, log the error and the value of the counter variable.
+function flatten(arr) {
+    if (!Array.isArray(arr)) {
+      //base case: if it is not an array and therefore already flat
+      return arr;
+    } else {
+      let result = [];
+      for (let i = 0; i < arr.length; i++) {
+        let flattened = flatten(arr[i]);
+        result = result.concat(flattened);
+      }
+      return result;
+    }
+  }
+  console.log(flatten([1, 2, [3, 4]]));
+  
